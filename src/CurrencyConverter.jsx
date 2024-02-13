d7a8ecfbdf30a1ae3b42045369d32cad62e08d7e
Yulik19 <juikhidukova@yandex.ru>
2024-02-13 11:17:14 +0300

    Removed commented lines from the code

--- src/CurrencyConverter.jsx
+++ src/CurrencyConverter.jsx
@@ -9,17 +9,16 @@ const CurrencyConverter = () => {
   const [amount, setAmount] = useState(0)
   const [convertedAmount, setConvertedAmount] = useState(0)
 
-  // Загрузка списка доступных валют из API
   useEffect(() => {
     const fetchCurrencies = async () => {
       try {
-        const response = await axios.get('https://api.exchangerate-api.com/v4/latest/USD')
+        const response = await axios.get('https://v6.exchangerate-api.com/v6/c5ea10b0c15573ca4f432035/latest/USD')
         const data = response.data
-        const currencyList = Object.keys(data.rates)
+        const currencyList = Object.keys(data.conversion_rates)
         setCurrencies(currencyList)
         setFromCurrency(currencyList[0])
         setToCurrency(currencyList[1])
-        setExchangeRate(data.rates[currencyList[1]])
+        setExchangeRate(data.conversion_rates[currencyList[1]])
       } catch (error) {
         console.error('Error fetching currencies:', error)
       }
@@ -28,13 +27,12 @@ const CurrencyConverter = () => {
     fetchCurrencies()
   }, [])
 
-  // Обновление курса обмена при изменении выбранных валют
   useEffect(() => {
     const fetchExchangeRate = async () => {
       try {
-        const response = await axios.get(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+        const response = await axios.get(` https://v6.exchangerate-api.com/v6/c5ea10b0c15573ca4f432035/latest/${fromCurrency}`)
         const data = response.data
-        setExchangeRate(data.rates[toCurrency])
+        setExchangeRate(data.conversion_rates[toCurrency])
       } catch (error) {
         console.error('Error fetching exchange rate:', error)
       }
@@ -43,29 +41,25 @@ const CurrencyConverter = () => {
     fetchExchangeRate()
   }, [fromCurrency, toCurrency])
 
-  // Обработчик изменения выбранной валюты "из"
   const handleFromCurrencyChange = (event) => {
     setFromCurrency(event.target.value)
   }
 
-  // Обработчик изменения выбранной валюты "в"
   const handleToCurrencyChange = (event) => {
     setToCurrency(event.target.value)
   }
 
-  // Обработчик изменения суммы
   const handleAmountChange = (event) => {
-    setAmount(event.target.value)
+    const value = event.target.value.replace(/\D/g, '')
+    setAmount(value)
   }
 
-  // Обработчик нажатия на кнопку "Поменять местами"
   const handleSwapCurrencies = () => {
     const temp = fromCurrency
     setFromCurrency(toCurrency)
     setToCurrency(temp)
   }
 
-  // Выполнение конвертации валют
   useEffect(() => {
     setConvertedAmount((amount * exchangeRate).toFixed(2))
   }, [amount, exchangeRate])
